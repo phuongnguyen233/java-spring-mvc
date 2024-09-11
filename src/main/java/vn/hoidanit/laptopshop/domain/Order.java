ea9bd83791d4a6da3dedfdc16eb272329fdc0969
NguyenMinhPhuong23 <phuongminhnguyen123a@gmail.com>
2024-09-11 22:22:53 +0700

    day8

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,11 +1,13 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "orders")
@@ -17,6 +19,8 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+    @OneToMany(mappedBy = "order")
+    List<OrderDetail> orderDetails;
     public long getId() {
         return id;
     }
--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -1,9 +1,11 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "products")
@@ -20,6 +22,7 @@ public class Product {
     private long sold;
     private String factory;
     private String target;
+    
     public long getId() {
         return id;
     }
